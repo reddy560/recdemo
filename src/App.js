70ec9a3247e7320e7eb6b5c1a3d452f2255aeb81
broy-vec <irsr560@gmail.com>
2024-11-12 18:24:08 +0530

    sddssd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Home from "./components/Home"
+import Gc from "./components/Gc"
+import Nav from "./components/Nav"
+import Login from "./components/Login"
+import Reg from "./components/Reg"
+import { useState } from "react"
+import Addprod from "./components/Addprod"
+import Cart from "./components/Cart"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let App=()=>{
+  let [contr,setContr]=useState({"token":"","uid":"","name":"","islogin":false,"isadmin":false,"carttotal":0
+
+
+})
+ let fun=(objs)=>{
+  console.log(objs)
+  setContr({...objs})
+ }
+ let obj={"contr":contr,"stateupdate":fun}
+  return(
+    <BrowserRouter>
+    <Gc.Provider value={obj}>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/reg" element={<Reg/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/add" element={<Addprod/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+
+
+      </Routes>
+
+
+    </Gc.Provider>
+    
+    </BrowserRouter>
+   
+  )
+
 }
-
-export default App;
+export default App
